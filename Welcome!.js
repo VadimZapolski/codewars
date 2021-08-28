67b98d2ca18846000ce309830477bf15159dd766
Vadim <64534235+VadimZapolski@users.noreply.github.com>
2021-08-28 12:21:08 +0300

    8 kyu

---
+++ Welcome!.js
@@ -0,0 +1,24 @@
+function greet(language) {
+  switch (language) {
+case 'english' : return 'Welcome'
+case 'czech' : return 'Vitejte'
+case 'danish' : return 'Velkomst'
+case 'dutch' : return 'Welkom'
+case 'estonian' : return 'Tere tulemast'
+case 'finnish' : return 'Tervetuloa'
+case 'flemish' : return 'Welgekomen'
+case 'french' : return 'Bienvenue'
+case 'german' : return 'Willkommen'
+case 'irish' : return 'Failte'
+case 'italian' : return 'Benvenuto'
+case 'latvian' : return 'Gaidits'
+case 'lithuanian' : return 'Laukiamas'
+case 'polish': return 'Witamy'
+case 'spanish' : return 'Bienvenido'
+case 'swedish' : return 'Valkommen'
+case 'welsh': return 'Croeso' 
+case 'IP_ADDRESS_INVALID' : return 'Welcome'
+case 'IP_ADDRESS_NOT_FOUND' : return 'Welcome'
+case 'IP_ADDRESS_REQUIRED' : return 'Welcome'
+  }
+}
