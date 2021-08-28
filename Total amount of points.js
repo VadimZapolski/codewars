4f673e40644b469ee12f830d223aa9682f375de4
Vadim <64534235+VadimZapolski@users.noreply.github.com>
2021-08-28 11:21:55 +0300

    8 kyu

--- Total amount of points.js
+++ Total amount of points.js
@@ -1,7 +1,6 @@
 function points(games) {
   let teamScore = 0;
 for (i=0; i<games.length; i++) {
-
   let home = parseInt(games[i].charAt(0));
   let away = parseInt(games[i].charAt(2));
   if (home > away)
