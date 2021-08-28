b0cb6102aaf58aa4ff2f336ac23d4c385ba5b51c
Vadim <64534235+VadimZapolski@users.noreply.github.com>
2021-08-28 11:46:46 +0300

    8 kyu

---
+++ Drink about.js
@@ -0,0 +1,17 @@
+function peopleWithAgeDrink(old) {
+  if(old<14) {
+    return "drink toddy"
+  }
+  if(old<18) {
+    return "drink coke"
+  }
+  if(old<19) {
+    return "drink beer"
+  }
+  if(old<21) {
+    return "drink beer"
+  }
+  if(old>20) {
+    return "drink whisky"
+  }
+};
