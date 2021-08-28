b3a95b52af287310ee3edbad867fdb08f27aea12
Vadim <64534235+VadimZapolski@users.noreply.github.com>
2021-08-28 12:50:44 +0300

    8 kyu

---
+++ For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,8 @@
+let quote = function(fighter) {
+  let x = fighter.toLowerCase()
+  if(x === 'conor mcgregor') {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  }if(x === 'george saint pierre') {
+    return "I am not impressed by your performance."
+  }
+};
