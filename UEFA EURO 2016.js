f45c70a8c42aacdb342e6b6a81cafc4dd80e065c
Vadim <64534235+VadimZapolski@users.noreply.github.com>
2021-08-28 12:08:11 +0300

    8 kyu

---
+++ UEFA EURO 2016.js
@@ -0,0 +1,11 @@
+function uefaEuro2016(teams, scores){
+  if(scores[0] > scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+  }
+  if(scores[0] < scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+  }
+  if(scores[0] == scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+  }
+}
